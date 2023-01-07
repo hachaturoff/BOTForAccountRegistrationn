436627f22e8c2f33b842be8a40e1fc76f3a52cfb
hachaturoff <oleg.hachaturoff@mail.ru>
2023-01-07 19:57:28 +0300

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,36 @@
+//
+//
+// const puppeteer = require('puppeteer');
+//
+// const url = 'https://dolf.finance/';
+//
+// async function fetchProductList(url) {
+//     const browser = await puppeteer.launch({
+//
+//         headless: true, // false: enables one to view the Chrome instance in action
+//         defaultViewport: null, // (optional) useful only in non-headless mode
+//     });
+//     const page = await browser.newPage();
+//
+//     await page.goto(url,
+//         // { waitUntil: 'networkidle2' }
+//     );
+//     await page.type('#root > div.header-area.header-bg > div > div > div:nth-child(1) > div > form > div > input', 'oe4g@gmail.com')
+//
+//     await page.click('#root > div.header-area.header-bg > div > div > div:nth-child(1) > div > form > div > div > button')
+//
+//     await page.waitForSelector('#waitlist-number > form > span')
+//
+//     const data = await page.evaluate(() => {
+//         return {
+//             ref: document.querySelector('.number-form-control').textContent,
+//             refLink: document.querySelector('.number-copy-control').value,
+//         }
+//     })
+//
+//     await page.screenshot({path:'scr.png'} )
+//     await browser.close()
+//     return data
+// }
+//
+// fetchProductList(url);
---
+++ tests/example.spec.js
@@ -0,0 +1,71 @@
+// @ts-check
+const {test} = require('@playwright/test');
+import fs from 'fs';
+
+test('New test ', async ({ page }) => {
+
+  let mail = ['oe2g@gmail.com','oe78g@gmail.com']
+
+  let mailRef = ['oe23g@gmail.com', 'oe34g@gmail.com', 'oe33g@gmail.com','oe2343g@gmail.com', 'oe4534g@gmail.com', 'oe3336g@gmail.com']
+
+  let BD = []
+  let inc = -1
+
+  for (let i = 0; i < mail.length ; i++) {
+
+    await page.goto('https://dolf.finance/')
+
+    await page.type('#root > div.header-area.header-bg > div > div > div:nth-child(1) > div > form > div > input', mail[i])
+
+    await page.click('#root > div.header-area.header-bg > div > div > div:nth-child(1) > div > form > div > div > button')
+
+    await page.waitForSelector('#waitlist-number > form > span')
+
+    const data = await page.evaluate(() => {
+
+      return {
+
+        ref: document.querySelector('.number-form-control').textContent,
+        refLink: document.querySelector('.number-copy-control').value,
+
+      }
+    })
+    data.email = mail[i]
+
+    await page.screenshot({ path: `example${i}.png` });
+
+    BD.push(data)
+
+      for (let ri = 0; ri <= 2; ri++) {
+        inc++
+
+        await page.goto(`https://${BD[i].refLink}/`)
+
+        await page.type('#root > div.header-area.header-bg > div > div > div:nth-child(1) > div > form > div > input', mailRef[inc])
+
+        await page.click('#root > div.header-area.header-bg > div > div > div:nth-child(1) > div > form > div > div > button')
+
+        await page.waitForSelector('#waitlist-number > form > span')
+
+        const data = await page.evaluate(() => {
+
+          return {
+
+            ref: document.querySelector('.number-form-control').textContent,
+            refLink: document.querySelector('.number-copy-control').value,
+
+          }
+        })
+        data.email = mailRef[inc]
+
+        await page.screenshot({ path: `example${ri}.png` });
+        console.log(inc)
+        BD.push(data)
+      }
+  }
+
+  console.log(BD)
+
+  fs.writeFileSync('fileWithArray.txt', JSON.stringify(BD), 'utf-8');
+
+});
